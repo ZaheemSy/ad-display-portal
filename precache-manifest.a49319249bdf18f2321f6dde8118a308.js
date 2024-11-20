937895613d38777f8eb9ca5aca4dc7deb6fe7842
ZaheemSE3 <zaheemse@gmail.com>
2024-11-20 09:54:42 +0400

    Updates

---
+++ precache-manifest.a49319249bdf18f2321f6dde8118a308.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e977077b230cb0c157ae5ca8780c17",
+    "url": "/ad-display-portal/index.html"
+  },
+  {
+    "revision": "5e2496a133608b70c7ed",
+    "url": "/ad-display-portal/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "abbc8f630761d337686e",
+    "url": "/ad-display-portal/static/js/2.c4996e9c.chunk.js"
+  },
+  {
+    "revision": "5e2496a133608b70c7ed",
+    "url": "/ad-display-portal/static/js/main.4a3fc0c6.chunk.js"
+  },
+  {
+    "revision": "f1e4a238f85e9b1f4531",
+    "url": "/ad-display-portal/static/js/runtime~main.b05952b4.js"
+  }
+]);
